dcfdd29984efe159ab6ca035a7b8f2c6b846af29
Johann Laur <jl.01@icloud.com>
2020-03-30 23:27:48 +0200

    Formatting & prefer-const

--- index.js
+++ index.js
@@ -5,156 +5,154 @@
  * @version 1.0
  */
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client } = require("discord.js"),
+	config = require("./config.json"),
+	Enmap = require("enmap");
+
+const client = new Client();
 
 // run module that handles messages (client#messageEvent)
 require("./messageHandler")(client);
-const config = require("./config.json");
 
-const Enmap = require("enmap");
+// Enmap for easy db management
 client.stats = new Enmap({ name: "stats", autoFetch: true, fetchAll: false });
+client.voiceStats = new Enmap({ name: "voiceStats", autoFetch: true, fetchAll: false });
+client.messageStats = new Enmap({ name: "messageStats", autoFetch: true, fetchAll: false });
 
 client.on("ready", () => {
-  console.log("Bot ready");
-  // array of users (id) who agreed
-  if (!client.stats.has("usersToLog")) client.stats.set("usersToLog", []);
-  client.user.setActivity(`${config.prefix}agree`, { type: "LISTENING" });
+	console.log("Client Ready");
+	// array of users (id) who agreed
+	if (!client.stats.has("usersToLog")) client.stats.set("usersToLog", []);
+	client.user.setActivity(`${config.prefix}agree`, { type: "LISTENING" });
 });
 
 client.on("presenceUpdate", async (oldMember, newMember) => {
-  //otherwise enmap database is full of boring bot infos
-  if (newMember.user.bot || oldMember.user.bot) return;
-  // return if user is not in array of users that agreed
-  if (client.stats.get("usersToLog").indexOf(oldMember.id) == -1) return;
-  let id = await newMember.user.id;
+	//otherwise enmap database is full of boring bot infos
+	if (newMember.user.bot || oldMember.user.bot) return;
+	// return if user is not in array of users that agreed
+	if (client.stats.get("usersToLog").includes(newMember.user.id)) return;
+	let id = newMember.user.id;
 
-  //saves game and start-timestamp to enmap if a user starts playing and did not play anything before
-  if (!oldMember.presence.game && newMember.presence.game) {
-    client.stats.set(`${id}-started`, {
-      game: newMember.presence.game.name,
-      started: new Date().getTime()
-    });
-    console.log(newMember.user.tag, "started game:", newMember.presence.game.name);
-  }
+	//saves game and start-timestamp to enmap if a user starts playing and did not play anything before
+	if (!oldMember.presence.game && newMember.presence.game)
+		client.stats.set(`${id}-started`, {
+			game: newMember.presence.game.name,
+			started: new Date().getTime()
+		});
 
-  //saves timestamps and seconds to enmap (after finishing the game)
-  if (oldMember.presence.game && !newMember.presence.game) {
-    if (!client.stats.has(`${id}-started`)) return; //return starttime was not saved (f.ex. because of bot downtime or we)
-    let now = new Date();
-    let gameE = client.stats.get(`${id}-started`);
-    let time = (now.getTime() - gameE.started) / 1000;
-    if (!client.stats.has(`${id}-${gameE.game}`)) client.stats.set(`${id}-${gameE.game}`, 0);
-    if (!client.stats.has(`${id}-${gameE.game}-times`))
-      client.stats.set(`${id}-${gameE.game}-times`, []);
-    client.stats.math(`${id}-${gameE.game}`, "+", time); //add played seconds to counter and save start & end time in obj-array
-    client.stats.push(`${id}-${gameE.game}-times`, {
-      start: new Date(gameE.started),
-      end: now
-    });
-    client.stats.delete(`${id}-started`); //delete start key to prevent errors later
-  }
+	//saves timestamps and seconds to enmap (after finishing the game)
+	if (oldMember.presence.game && !newMember.presence.game) {
+		if (!client.stats.has(`${id}-started`)) return; //return starttime was not saved (f.ex. because of bot downtime or we)
+		const now = new Date(),
+			gameE = client.stats.get(`${id}-started`),
+			time = (now.getTime() - gameE.started) / 1000;
+		if (!client.stats.has(`${id}-${gameE.game}`)) client.stats.set(`${id}-${gameE.game}`, 0);
+		if (!client.stats.has(`${id}-${gameE.game}-times`))
+			client.stats.set(`${id}-${gameE.game}-times`, []);
+		client.stats.math(`${id}-${gameE.game}`, "+", time); //add played seconds to counter and save start & end time in obj-array
+		client.stats.push(`${id}-${gameE.game}-times`, {
+			start: new Date(gameE.started),
+			end: now
+		});
+		client.stats.delete(`${id}-started`); //delete start key to prevent errors later
+	}
 
-  //almost same stuff like above - triggers when the game presence changes
-  if (
-    oldMember.presence.game &&
-    newMember.presence.game &&
-    oldMember.presence.game != newMember.presence.game
-  ) {
-    if (!client.stats.has(`${id}-started`))
-      return client.stats.set(`${id}-started`, {
-        game: newMember.presence.game.name,
-        started: new Date().getTime()
-      });
-    let now = new Date();
-    let gameE = client.stats.get(`${id}-started`);
-    let time = (now.getTime() - gameE.started) / 1000;
-    if (!client.stats.has(`${newMember.user.id}-${gameE.game}`))
-      client.stats.set(`${newMember.user.id}-${gameE.game}`, 0);
-    if (!client.stats.has(`${id}-${gameE.game}-times`))
-      client.stats.set(`${id}-${gameE.game}-times`, []);
-    client.stats.math(`${newMember.user.id}-${gameE.game}`, "+", time);
-    client.stats.push(`${id}-${gameE.game}-times`, {
-      start: new Date(gameE.started),
-      end: now
-    });
-    client.stats.delete(`${id}-started`);
-  }
+	//almost same stuff like above - triggers when the game presence changes
+	if (
+		oldMember.presence.game &&
+		newMember.presence.game &&
+		oldMember.presence.game != newMember.presence.game
+	) {
+		if (!client.stats.has(`${id}-started`))
+			return client.stats.set(`${id}-started`, {
+				game: newMember.presence.game.name,
+				started: new Date().getTime()
+			});
+		const now = new Date(),
+			gameE = client.stats.get(`${id}-started`),
+			time = (now.getTime() - gameE.started) / 1000;
+		if (!client.stats.has(`${newMember.user.id}-${gameE.game}`))
+			client.stats.set(`${newMember.user.id}-${gameE.game}`, 0);
+		if (!client.stats.has(`${id}-${gameE.game}-times`))
+			client.stats.set(`${id}-${gameE.game}-times`, []);
+		client.stats.math(`${newMember.user.id}-${gameE.game}`, "+", time);
+		client.stats.push(`${id}-${gameE.game}-times`, {
+			start: new Date(gameE.started),
+			end: now
+		});
+		client.stats.delete(`${id}-started`);
+	}
 
-  //function to save online and idle time of a user if their presence status changes
-  if (oldMember.presence.status != newMember.presence.status) {
-    console.log(`Member status changed: ${newMember.presence.status} (${oldMember.user.tag})`);
-    //dnd = online
-    if (newMember.presence.status == "online" || newMember.presence.status == "dnd") {
-      let now = new Date();
-      if (client.stats.has(`${id}-wentIdle`)) {
-        //saves idle time if presence changed from idle to online (wentIdle date was saved before)
-        if (!client.stats.has(`${id}-idleTime`)) client.stats.set(`${id}-idleTime`, 0);
-        let wentIdle = await client.stats.get(`${id}-wentIdle`);
-        client.stats.math(`${id}-idleTime`, "+", (now.getTime() - wentIdle) / 1000);
-        client.stats.delete(`${id}-wentIdle`);
-      }
-      client.stats.set(`${id}-wentOn`, now.getTime()); //saves timestamp to get online time later
-    }
-    //same with idling :D
-    if (newMember.presence.status == "idle") {
-      let now = new Date();
-      if (client.stats.has(`${id}-wentOn`)) {
-        if (!client.stats.has(`${id}-onlineTime`)) client.stats.set(`${id}-onlineTime`, 0);
-        let wentOn = await client.stats.get(`${id}-wentOn`);
-        client.stats.math(`${id}-onlineTime`, "+", (now.getTime() - wentOn) / 1000);
-        client.stats.delete(`${id}-wentOn`);
-      }
-      client.stats.set(`${id}-wentIdle`, now.getTime());
-    }
-    //saves idle or online (/dnd) time after presence status changes to off
-    if (newMember.presence.status == "offline") {
-      let now = new Date();
-      if (client.stats.has(`${id}-wentOn`)) {
-        if (!client.stats.has(`${id}-onlineTime`)) client.stats.set(`${id}-onlineTime`, 0);
-        let wentOn = await client.stats.get(`${id}-wentOn`);
-        client.stats.math(`${id}-onlineTime`, "+", (now.getTime() - wentOn) / 1000);
-        client.stats.delete(`${id}-wentOn`);
-      }
-      if (client.stats.has(`${id}-wentIdle`)) {
-        if (!client.stats.has(`${id}-idleTime`)) client.stats.set(`${id}-idleTime`, 0);
-        let wentIdle = await client.stats.get(`${id}-wentIdle`);
-        client.stats.math(`${id}-idleTime`, "+", (now.getTime() - wentIdle) / 1000);
-        client.stats.delete(`${id}-wentIdle`);
-      }
-    }
-  }
+	//function to save online and idle time of a user if their presence status changes
+	if (oldMember.presence.status != newMember.presence.status) {
+		console.log(`Member status changed: ${newMember.presence.status} (${oldMember.user.tag})`);
+		//dnd = online
+		if (newMember.presence.status == "online" || newMember.presence.status == "dnd") {
+			const now = new Date();
+			if (client.stats.has(`${id}-wentIdle`)) {
+				//saves idle time if presence changed from idle to online (wentIdle date was saved before)
+				if (!client.stats.has(`${id}-idleTime`)) client.stats.set(`${id}-idleTime`, 0);
+				let wentIdle = await client.stats.get(`${id}-wentIdle`);
+				client.stats.math(`${id}-idleTime`, "+", (now.getTime() - wentIdle) / 1000);
+				client.stats.delete(`${id}-wentIdle`);
+			}
+			client.stats.set(`${id}-wentOn`, now.getTime()); //saves timestamp to get online time later
+		}
+		//same with idling :D
+		if (newMember.presence.status == "idle") {
+			const now = new Date();
+			if (client.stats.has(`${id}-wentOn`)) {
+				if (!client.stats.has(`${id}-onlineTime`)) client.stats.set(`${id}-onlineTime`, 0);
+				let wentOn = await client.stats.get(`${id}-wentOn`);
+				client.stats.math(`${id}-onlineTime`, "+", (now.getTime() - wentOn) / 1000);
+				client.stats.delete(`${id}-wentOn`);
+			}
+			client.stats.set(`${id}-wentIdle`, now.getTime());
+		}
+		//saves idle or online (/dnd) time after presence status changes to off
+		if (newMember.presence.status == "offline") {
+			const now = new Date();
+			if (client.stats.has(`${id}-wentOn`)) {
+				if (!client.stats.has(`${id}-onlineTime`)) client.stats.set(`${id}-onlineTime`, 0);
+				let wentOn = await client.stats.get(`${id}-wentOn`);
+				client.stats.math(`${id}-onlineTime`, "+", (now.getTime() - wentOn) / 1000);
+				client.stats.delete(`${id}-wentOn`);
+			}
+			if (client.stats.has(`${id}-wentIdle`)) {
+				if (!client.stats.has(`${id}-idleTime`)) client.stats.set(`${id}-idleTime`, 0);
+				let wentIdle = await client.stats.get(`${id}-wentIdle`);
+				client.stats.math(`${id}-idleTime`, "+", (now.getTime() - wentIdle) / 1000);
+				client.stats.delete(`${id}-wentIdle`);
+			}
+		}
+	}
 });
 
-client.on("voiceStateUpdate", (oldMember, newMember) => {
-  // same structure as in presenceUpdate
-  if (newMember.user.bot || oldMember.user.bot) return;
-  if (client.stats.get("usersToLog").indexOf(oldMember.id) == -1) return;
+client.on("voiceStateUpdate", async (oldMember, newMember) => {
+	// same structure as in presenceUpdate
+	if (newMember.user.bot || oldMember.user.bot) return;
+	if (client.stats.get("usersToLog").indexOf(oldMember.id) == -1) return;
 
-  let id = await newMember.user.id;
+	const id = await newMember.user.id;
 
-  if (!oldMember.voiceChannel && newMember.voiceChannel) {
-    client.stats.set(`${id}-startedVC`, {
-      time: new Date().getTime(),
-      channel: newMember.voiceChannel.id
-    });
-    //if (!client.stats.has(`${newMember.voiceChannelID}-VC`))
-    //client.stats.set(`${newMember.voiceChannelID}-VC`, 0);
-    console.log(newMember.user.tag, "joined channel");
-    console.log(newMember.voiceChannel.members.map(m => m.id));
-  }
+	if (!oldMember.voiceChannel && newMember.voiceChannel)
+		client.stats.set(`${id}-startedVC`, {
+			time: new Date().getTime(),
+			channel: newMember.voiceChannel.id
+		});
+	//if (!client.stats.has(`${newMember.voiceChannelID}-VC`))
+	//client.stats.set(`${newMember.voiceChannelID}-VC`, 0);
 
-  if (oldMember.voiceChannel && !newMember.voiceChannel) {
-    if (!client.stats.has(`${id}-startedVC`)) return;
-    console.log(`${newMember.user.tag} left channel`);
-    let now = new Date();
-    let startedVC = await client.stats.get(`${id}-startedVC`).time;
-    if (!client.stats.has(`${id}-voiceTime`)) client.stats.set(`${id}-voiceTime`, 0);
-    client.stats.math(`${id}-voiceTime`, "+", (now.getTime() - startedVC) / 1000);
-    //if (oldMember.voiceChannel.members.array().length < 1)
-    //client.stats.math(`${oldMember.voiceChannel.id}-VCT`, "+", time);
-    client.stats.delete(`${id}-startedVC`);
-  }
-})
+	if (oldMember.voiceChannel && !newMember.voiceChannel) {
+		if (!client.stats.has(`${id}-startedVC`)) return;
+		const now = new Date(),
+			startedVC = await client.stats.get(`${id}-startedVC`).time;
+		if (!client.stats.has(`${id}-voiceTime`)) client.stats.set(`${id}-voiceTime`, 0);
+		client.stats.math(`${id}-voiceTime`, "+", (now.getTime() - startedVC) / 1000);
+		//if (oldMember.voiceChannel.members.array().length < 1)
+		//client.stats.math(`${oldMember.voiceChannel.id}-VCT`, "+", time);
+		client.stats.delete(`${id}-startedVC`);
+	}
+});
 
 client.login(config.token);
--- messageHandler.js
+++ messageHandler.js
@@ -8,53 +8,55 @@
  * @requires config.json with prefix
  */
 
-const Discord = require("discord.js");
-const config = require("./config.json");
-let msg = new Discord.RichEmbed()
-  .setColor(7506394)
-  .setTitle("Logging of user data")
-  .setDescription(
-    "This bot collects user data about your discord usage by using the presence status in discord.\nIt only saves your message id with your online/idle time and game activitys.\nOf course this can not be done by default so if you agree just react with :white_check_mark:!"
-  )
-  .setTimestamp();
+const Discord = require("discord.js"),
+	config = require("./config.json");
+
+const msg = new Discord.MessageEmbed()
+	.setColor(7506394)
+	.setTitle("Logging of user data")
+	.setDescription(
+		"This bot collects user data about your discord usage by using the presence status in discord.\nIt only saves your message id with your online/idle time and game activitys.\nOf course this can not be done by default so if you agree just react with :white_check_mark:!"
+	)
+	.setTimestamp();
 
 module.exports = client => {
-  client.on("message", message => {
-    // command to create a reaction collector for agreement
-    if (message.content.startsWith(`${config.prefix}collector`)) {
-      if (!message.member.hasPermission("ADMINISTRATOR")) return message.delete();
+	client.on("message", message => {
+		// command to create a reaction collector for agreement
+		if (message.content.startsWith(`${config.prefix}collector`)) {
+			if (!message.member.hasPermission("ADMINISTRATOR")) return message.delete();
 
-      message.channel.send(msg).then(m => {
-        m.react("✅");
-        const filter = (reaction, user) => reaction.emoji.name === "✅";
-        const collector = m.createReactionCollector(filter);
-        collector.on("collect", (element, collector) => {
-          let users = element.users.map(x => x.id);
-          users.forEach(async element => {
-            await client.stats.push("usersToLog", element);
-          });
-        });
-      });
-      message.delete();
-    }
+			message.channel.send(msg).then(m => {
+				m.react("✅");
+				const filter = (reaction, user) => reaction.emoji.name === "✅";
+				const collector = m.createReactionCollector(filter);
+				collector.on("collect", (element, collector) => {
+					const users = element.users.map(x => x.id);
+					users.forEach(async element => {
+						await client.stats.push("usersToLog", element);
+					});
+				});
+			});
+			message.delete();
+		}
 
-    // simple way to agree and get pushed into "usersToLog" array
-    if (message.content.startsWith(`${config.prefix}agree`)) {
-      let userID = message.author.id;
-      client.stats.push("usersToLog", userID);
-      message.react("✅");
-    }
+		// simple way to agree and get pushed into "usersToLog" array
+		if (message.content.startsWith(`${config.prefix}agree`)) {
+			client.stats.push("usersToLog", message.author.id);
+			message.react("✅");
+		}
 
-    // command to revoke agreement
-    if (message.content.startsWith(`${config.prefix}revoke`)) {
-      let userID = message.author.id;
-      let arr = client.stats.get("usersToLog");
-      let index = arr.indexOf(userID);
-      if (index == -1)
-        return message.channel.send(`You did not agree yet! (Just type ${config.prefix}agree)`);
-      arr.splice(index, index);
-      client.stats.set("usersToLog", arr);
-      message.react("✅");
-    }
-  });
+		// command to revoke agreement
+		if (message.content.startsWith(`${config.prefix}revoke`)) {
+			const userID = message.author.id,
+				arr = client.stats.get("usersToLog"),
+				index = arr.indexOf(userID);
+			if (index == -1)
+				return message.channel.send(
+					`You did not agree yet! (Just type ${config.prefix}agree)`
+				);
+			arr.splice(index, index);
+			client.stats.set("usersToLog", arr);
+			message.react("✅");
+		}
+	});
 };
